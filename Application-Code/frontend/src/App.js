2ddf1338aa653162ac9f72262794ce9f26ab3ac9
Vicky kumawat <vickykumawat766@gmail.com>
2024-12-03 14:28:32 +0530

    Update App.js
    ghf

--- Application-Code/frontend/src/App.js
+++ Application-Code/frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List</h1>
+                    <h1>Sportz Interactive</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
